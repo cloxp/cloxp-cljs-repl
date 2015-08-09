71fde4b8ede78162e40fe18ca078423db7039dc5
Robert Krahn <robert.krahn@gmail.com>
2015-08-10 00:20:50 +0200

    making the workspace / eval stuff work again

--- cljs-src/rksm/cloxp_cljs_repl/workspace_tester.cljs
+++ cljs-src/rksm/cloxp_cljs_repl/workspace_tester.cljs
@@ -1,5 +1,15 @@
 (ns rksm.cloxp-cljs-repl.workspace-tester
-  (:require [rksm.cloxp-com.cloxp-client :as cloxp]))
+  (:require [rksm.cloxp-com.cloxp-client :as cloxp]
+            [rksm.cloxp-com.messenger :as messenger]
+            [rksm.cloxp-cljs-repl.eval :as repl]))
 
 ; (cloxp/start )
-(cloxp/start :host "localhost" :port 8084)
+(cloxp/start
+ :host "localhost" :port 8084
+ :then-do (fn [con]
+            (try 
+              (do
+                (messenger/add-service con "eval-js" repl/eval-js-service)
+                (messenger/add-service con "load-js" repl/load-js-service)
+                (println "added or what???"))
+              (catch js/Error e (println e)))))
--- src/rksm/cloxp_cljs_repl/core.clj
+++ src/rksm/cloxp_cljs_repl/core.clj
@@ -2,6 +2,11 @@
   (:require [cljs.repl :as repl]
             [cljs.analyzer :as ana]
             [cljs.env :as env]
+            [cljs.tagged-literals :as tags]
+            [clojure.tools.reader :as reader]
+            [clojure.tools.reader.reader-types :as readers]
+            [rksm.cloxp-repl :as cloxp-repl]
+            [rksm.cloxp-cljs.analyzer :as cloxp-ana]
             [rksm.cloxp-com.server :as server]
             [rksm.cloxp-com.messenger :as msg]
             [clojure.core.async :refer [>!! <!! >! <! go]]))
@@ -41,23 +46,73 @@
     (->CloxpCljsReplEnv server client-id)
     (throw (Exception. (str "No cljs-repl connection for client " client-id " found")))))
 
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+(defn read-cljs-string
+  "opts: {:file :line-offset :column-offset :features}"
+  [cljs-string & [cljs-ns opts]]
+  (let [opts (if (contains? opts :features)
+               opts (assoc opts :features #{:cljs}))]
+    (binding [*ns* (create-ns (or cljs-ns ana/*cljs-ns*))
+              reader/*data-readers* tags/*cljs-data-readers*
+              reader/*alias-map* (apply merge
+                                   ((juxt :requires :require-macros)
+                                    (cloxp-ana/with-compiler
+                                      (ana/get-namespace (ns-name *ns*)))))]
+      (cloxp-repl/read-clj-string cljs-string *ns* opts))))
+
 (defn eval-cljs
-  ([form {:keys [target-id] :as opts}]
-   (let [env (if target-id
-               (repl-env-for-client target-id)
-               (default-repl-env))]
-     (eval-cljs form env (or opts {}))))
-  ([form cloxp-repl-env {:keys [ns-sym] :as opts}]
-   (let [c-env (or env/*compiler* (env/default-compiler-env))
+  "returns map of {:value :out :error}"
+  ([form opts]
+   (eval-cljs form nil opts))
+  ([form cloxp-repl-env {:keys [target-id ns-sym] :as opts}]
+   (let [opts (or opts {})
+         cloxp-repl-env (cond
+                          cloxp-repl-env cloxp-repl-env
+                          target-id (repl-env-for-client target-id)
+                          :default (default-repl-env))
+         c-env (or env/*compiler* (env/default-compiler-env))
          ana-env (merge (ana/empty-env) {:ns 'cljs.user})]
      (env/with-compiler-env c-env
-       (repl/evaluate-form cloxp-repl-env
-                           (assoc ana-env :ns (ana/get-namespace
-                                               (or ns-sym ana/*cljs-ns*)))
-                           "<cloxp-cljs-repl>"
-                           form
-                           identity ; wrap-fn
-                           opts)))))
+       (try
+         {:error nil
+          :out ""
+          :value (repl/evaluate-form cloxp-repl-env
+                                     (assoc ana-env :ns (ana/get-namespace
+                                                         (or ns-sym ana/*cljs-ns*)))
+                                     "<cloxp-cljs-repl>"
+                                     form
+                                     identity ; wrap-fn
+                                     opts)}
+         (catch Exception e {:error e
+                             :out ""
+                             :value nil}))))))
+
+(defn eval-cljs-string
+  ([expr opts]
+   (eval-cljs-string expr nil opts))
+  ([expr cloxp-repl-env {:keys [ns-sym file line-offset column-offset] :or {file (or *file* "NO_SOURCE_FILE")} :as opts}]
+   (->> (read-cljs-string expr ns-sym
+                          {:features #{:cljs}
+                           :line-offset (or line-offset cloxp-repl/*line-offset*)
+                           :column-offset (or column-offset cloxp-repl/*column-offset*)})
+     (map #(eval-read-obj cloxp-repl-env % (merge opts {:file file})))
+     doall)))
+
+(defn- eval-read-obj
+  "returns a map of :parsed :value :out and :error."
+  [cloxp-repl-env
+   {:keys [form] :as parsed}
+   & [{:keys [ns-sym line-offset throw-errors?]
+       :or {line-offset 0, throw-errors? false}
+       :as opts}]]
+  (let [add-to-meta {} ;(select-keys parsed [:line :column :source])
+        ; (eval-cljs (read-cljs-string expr ns-sym opts) cloxp-repl-env opts)
+        {:keys [error] :as result} (eval-cljs form cloxp-repl-env
+                                              (update-in opts [:add-meta] merge add-to-meta))]
+    (if (and error throw-errors?)
+      (throw error)
+      (assoc result :parsed parsed))))
 
 (defn load-namespace
   ([sym opts]
